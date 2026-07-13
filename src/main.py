4feef8332c9bf4014c0ac4967c9dff5dd89fa14c
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-13 10:32:48 +0200

    feat: add quick sort algorithm

--- src/main.py
+++ src/main.py
@@ -215,8 +215,41 @@ def merge_sort(elem_list):
 
     return merge(left_list, right_list)
 
-    
-mylist = [2,5,6,1,3,4]
-merge_sort(mylist)
-print(mylist)
+
+""" Sort a list in an ascendant order using the quick sort algorithm.
+It creates a copy of the original list.
+
+Args:
+    elem_list (list): List to sort.
+
+Returns:
+    list: Sorted list.
+"""
+def quick_sort(elem_list):
+    if len(elem_list) <= 1: 
+        return elem_list
+
+    pivot = elem_list[-1]
+    left_list = []
+    right_list = []
+
+    for i, el in enumerate(elem_list):
+        if i == len(elem_list) - 1: 
+            continue
+        if el < pivot: 
+            left_list.append(el)
+        else: 
+            right_list.append(el)
+
+    left_list = quick_sort(left_list)
+    right_list = quick_sort(right_list)
+
+    sorted_list = []
+    sorted_list.extend(left_list)
+    sorted_list.append(pivot)
+    sorted_list.extend(right_list)
+
+    return sorted_list
+
+print(quick_sort([5,5,5]))
 
